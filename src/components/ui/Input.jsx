59adc588a6759330db4c7702ac8cbce00f4cdfd8
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-12-07 22:10:08 +0900

    Style signup page
    Style signup page.

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  width: 25em;
+  height: 7em;
+  margin-bottom: 1.5em;
+  color: #A0A0A0;
+
+  p {
+    font-size: 0.95em;
+  }
+`;
+
+const StyledInput = styled.input`
+  font-size: 1em;
+  font-weight: 400;
+  height: 3.75em;
+  padding: 1em;
+  border: ${(props) => (`1px solid${props.error ? '#FF424D' : '#D8D8D8'}`)};
+  color: #666666;
+
+  :focus {
+    font-size: 1em;
+    border: ${(props) => (`1px solid${props.error ? '#FF424D' : props.theme.colors.primary}`)};
+    outline: none;
+    color: #666666;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 700;
+  display: block;
+`;
+
+const Required = styled.span`
+  color: #FF424D;
+`;
+
+const Error = styled.p`
+  color: #FF424D;
+`;
+
+export default function Input({
+  name, label, type, value, maxLength = '', handleChange, message, errorMessage, required = false,
+}) {
+  return (
+    <Container>
+      <Label htmlFor={`input-${name}`}>
+        {label}
+        {required
+          ? (
+            <Required>
+              *
+            </Required>
+          )
+          : null}
+      </Label>
+      <StyledInput
+        type={type}
+        name={name}
+        id={`input-${name}`}
+        value={value}
+        error={errorMessage}
+        maxLength={maxLength}
+        onChange={handleChange}
+      />
+      {errorMessage
+        ? <Error>{errorMessage}</Error>
+        : <p>{message}</p>}
+    </Container>
+  );
+}
